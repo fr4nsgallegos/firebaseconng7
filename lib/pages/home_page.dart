6caf87ccefff0087b42e199fc70bded1c7270630
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-02-18 12:30:20 -0500

    inserciones en bd

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,10 +12,34 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-                onPressed: () {
-                  productsReference.get();
-                },
-                child: Text("Traer DATA"))
+              onPressed: () {
+                productsReference.get().then((value) {
+                  QuerySnapshot productCollection = value;
+                  List<QueryDocumentSnapshot> docs = productCollection.docs;
+                  docs.forEach((element) {
+                    print(element.id);
+                  });
+                });
+              },
+              child: Text("Traer DATA"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                productsReference.add({
+                  'nombre': 'Juan',
+                  'apellido': 'Portal',
+                });
+              },
+              child: Text("Insertar 1"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                productsReference.doc('bividis').set(
+                  {"tipo": "ropa", "color": "Blanco"},
+                );
+              },
+              child: Text("Insertar 2"),
+            ),
           ],
         ),
       ),
